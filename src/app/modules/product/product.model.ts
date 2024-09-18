edb0496e4ac69da0b2533c5cb6766ac2d6b5ae1b
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-09-18 21:37:42 +0600

    Product model created

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose'
+import { TProduct } from './product.interface'
+
+const productSchema = new Schema<TProduct>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Product name is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Product price is required'],
+    },
+    imageUrl: {
+      type: String,
+      required: [true, 'Product image URL is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Product description is required'],
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+      required: [true, 'Stock availability is required'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Create and export the Product model
+const Product = model<TProduct>('Product', productSchema)
+
+export default Product
